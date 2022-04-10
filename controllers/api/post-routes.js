028a0c0a2ecc5855f1178d339e7e2066c6a6ff5c
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-04-09 20:33:01 -0700

    updated comment and user routes, added home routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at'
     ],
@@ -41,7 +41,7 @@ router.get('/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at',
     ],
@@ -77,6 +77,7 @@ router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
+    content: req.body.content,
     user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
